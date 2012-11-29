f3d49acb9fb31c53ed980cba8c36a49977c59fcd
Luke Evans <levans002@gmail.com>
2012-11-28 17:09:33 -0700

    test

--- src/cs555/dht/node/PeerNode.java
+++ src/cs555/dht/node/PeerNode.java
@@ -228,7 +228,7 @@ public class PeerNode extends Node{
 
 	public void sendPredessessorRequest(Peer p, PredessesorRequest r) {
 		//Link sucessorLink = p.link; 
-		p.initLink();
+		//p.initLink();
 		p.link.sendData(r.marshall());
 	}
 
