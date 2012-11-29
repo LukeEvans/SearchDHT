307c2c8b39703f4a71ac12c279b67080c8034324
Luke Evans <levans002@gmail.com>
2012-11-28 17:22:21 -0700

    refresh time down

--- src/cs555/dht/utilities/Constants.java
+++ src/cs555/dht/utilities/Constants.java
@@ -42,7 +42,7 @@ public class Constants {
 	// Base path for data to be stored
 	public static final String base_path = "/tmp/evansl/dhtData/";
 	
-	public static final int Refresh_Time = 120;
+	public static final int Refresh_Time = 20;
 	
 	public static final boolean logging = true;
 	
